857932e56fd6811e4a97e4d2e630780b6988bb21
Artur Zabieglinski <AkuroNiariae@users.noreply.github.com>
2021-03-30 11:14:54 +0100

    Add Maven
    Add Maven

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,17 @@ public class Main {
 
 
     public static void main(String[] args) {
+
+        System.out.println("Walcome in procject \"Bartender\'s Handybook\" ");
+        System.out.println("Created by:");
         System.out.println("Drink 2 Code!");
+
+
+        System.out.print("Credits: Agnieszka Gutowska, Artur Zabiegliński,");
+        System.out.print("Przemysław Pettka, Sandra Kowalska.");
+
+
+
     }
 
 
