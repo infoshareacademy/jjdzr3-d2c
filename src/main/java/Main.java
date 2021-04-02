479242b01f90f5dca7475ca4016bfc734cbc5822
Agnieszka Gutowska <a.gutowska1@gmail.com>
2021-04-02 21:00:26 +0200

    JJDZR3D2C-8

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,9 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import drink.Drink;
+
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
 
 
@@ -8,5 +14,25 @@ public class Main {
         System.out.println("Drink 2 Code!");
         System.out.print("Credits: Agnieszka Gutowska, Artur Zabiegliński, ");
         System.out.print("Przemysław Pettka, Sandra Kowalska.");
-  }
+
+
+
+        System.out.print("Przemysław Pettka, Sandra Kowalska.\n \n \n" );
+
+        ObjectMapper objectMapper=new ObjectMapper();
+
+
+        File file = new File("src", "onedrink.json");
+
+        try {
+            Drink drink1 = objectMapper.readValue(file, Drink.class);
+            System.out.println(drink1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+
+    }
 }
--- src/main/java/drink/Drink.java
+++ src/main/java/drink/Drink.java
@@ -1,15 +1,216 @@
 package drink;
 
-
 public class Drink {
-    Integer drinkID;
-    String drinkName;
-    String drinkIngredient;
-    String drinkIngredientMeasure;
-    String dringInstruction;
-    Type typeAlkohol; // Drink alkoholowy lub bezalkoholowy - Enum
+    private Integer idDrink;
+    private String strDrink ;
+    private String strCategory;
+    private  String strAlcoholic;
+    private  String strGlass;
+    private String strInstructions;
+    private  String strInstructionsES;
+    private  String strIngredient1;
+    private  String strIngredient2;
+    private  String strIngredient3;
+    private  String strIngredient4;
+    private  String strIngredient5;
+    private String strMeasure1;
+    private String strMeasure2;
+    private String strMeasure3;
+    private String strMeasure4;
+    private String strMeasure5;
 
-    public Drink () {
+    private String typeAlcohol; // Drink alkoholowy lub bezalkoholowy - Enum
+    public Drink() {
+    }
 
+
+
+    public Drink (Integer drinkID, String strDrink, String strCategory, String strAlcoholic, String strGlass, String strInstructions, String strIngredient1, String strIngredient2, String strIngredient3,String strIngredient4, String strIngredient5, String strMeasure1 , String strMeasure2 , String strMeasure3 , String strMeasure4 , String strMeasure5 ) {
+        this.idDrink = drinkID;
+        this.strDrink = strDrink;
+        this.strCategory = strAlcoholic;
+        this.strAlcoholic=strAlcoholic;
+        this.strGlass= strGlass;
+        this.strInstructions= strInstructions;
+        this.strIngredient1= strIngredient1;
+        this.strIngredient2= strIngredient2;
+        this.strIngredient3= strIngredient3;
+        this.strIngredient4= strIngredient4;
+        this.strIngredient5= strIngredient5;
+        this.strMeasure1= strMeasure1;
+        this.strMeasure2= strMeasure2;
+        this.strMeasure3= strMeasure3;
+        this.strMeasure4= strMeasure4;
+        this.strMeasure5= strMeasure5;
+
+
+    }
+
+    @Override
+    public String toString() {
+        return "Drink{" +
+                "idDrink=" + idDrink +
+                ", strDrink='" + strDrink + '\'' +
+                ", strCategory='" + strCategory + '\'' +
+                ", strAlcoholic='" + strAlcoholic + '\'' +
+                ", strGlass='" + strGlass + '\'' +
+                ", strInstructions='" + strInstructions + '\'' +
+                ", strInstructionsES='" + strInstructionsES + '\'' +
+                ", strIngredient1='" + strIngredient1 + '\'' +
+                ", strIngredient2='" + strIngredient2 + '\'' +
+                ", strIngredient3='" + strIngredient3 + '\'' +
+                ", strIngredient4='" + strIngredient4 + '\'' +
+                ", strIngredient5='" + strIngredient5 + '\'' +
+                ", strMeasure1='" + strMeasure1 + '\'' +
+                ", strMeasure2='" + strMeasure2 + '\'' +
+                ", strMeasure3='" + strMeasure3 + '\'' +
+                ", strMeasure4='" + strMeasure4 + '\'' +
+                ", strMeasure5='" + strMeasure5 + '\'' +
+                ", typeAlcohol='" + typeAlcohol + '\'' +
+                '}';
+    }
+
+    public Integer getIdDrink() {
+        return idDrink;
+    }
+
+    public void setIdDrink(Integer idDrink) {
+        this.idDrink = idDrink;
+    }
+
+    public String getStrDrink() {
+        return strDrink;
+    }
+
+    public void setStrDrink(String strDrink) {
+        this.strDrink = strDrink;
+    }
+
+    public String getStrCategory() {
+        return strCategory;
+    }
+
+    public void setStrCategory(String strCategory) {
+        this.strCategory = strCategory;
+    }
+
+    public String getStrAlcoholic() {
+        return strAlcoholic;
+    }
+
+    public void setStrAlcoholic(String strAlcoholic) {
+        this.strAlcoholic = strAlcoholic;
+    }
+
+    public String getStrGlass() {
+        return strGlass;
+    }
+
+    public void setStrGlass(String strGlass) {
+        this.strGlass = strGlass;
+    }
+
+    public String getStrInstructions() {
+        return strInstructions;
+    }
+
+    public void setStrInstructions(String strInstructions) {
+        this.strInstructions = strInstructions;
+    }
+
+    public String getStrInstructionsES() {
+        return strInstructionsES;
+    }
+
+    public void setStrInstructionsES(String strInstructionsES) {
+        this.strInstructionsES = strInstructionsES;
+    }
+
+    public String getStrIngredient1() {
+        return strIngredient1;
+    }
+
+    public void setStrIngredient1(String strIngredient1) {
+        this.strIngredient1 = strIngredient1;
+    }
+
+    public String getStrMeasure1() {
+        return strMeasure1;
+    }
+
+    public void setStrMeasure1(String strMeasure1) {
+        this.strMeasure1 = strMeasure1;
+    }
+
+    public String getTypeAlcohol() {
+        return typeAlcohol;
+    }
+
+    public void setTypeAlcohol(String typeAlcohol) {
+        this.typeAlcohol = typeAlcohol;
+    }
+
+    public String getStrIngredient2() {
+        return strIngredient2;
+    }
+
+    public void setStrIngredient2(String strIngredient2) {
+        this.strIngredient2 = strIngredient2;
+    }
+
+    public String getStrIngredient3() {
+        return strIngredient3;
+    }
+
+    public void setStrIngredient3(String strIngredient3) {
+        this.strIngredient3 = strIngredient3;
+    }
+
+    public String getStrIngredient4() {
+        return strIngredient4;
+    }
+
+    public void setStrIngredient4(String strIngredient4) {
+        this.strIngredient4 = strIngredient4;
+    }
+
+    public String getStrIngredient5() {
+        return strIngredient5;
+    }
+
+    public void setStrIngredient5(String strIngredient5) {
+        this.strIngredient5 = strIngredient5;
+    }
+
+    public String getStrMeasure2() {
+        return strMeasure2;
+    }
+
+    public void setStrMeasure2(String strMeasure2) {
+        this.strMeasure2 = strMeasure2;
+    }
+
+    public String getStrMeasure3() {
+        return strMeasure3;
+    }
+
+    public void setStrMeasure3(String strMeasure3) {
+        this.strMeasure3 = strMeasure3;
+    }
+
+    public String getStrMeasure4() {
+        return strMeasure4;
+    }
+
+    public void setStrMeasure4(String strMeasure4) {
+        this.strMeasure4 = strMeasure4;
+    }
+
+    public String getStrMeasure5() {
+        return strMeasure5;
+    }
+
+    public void setStrMeasure5(String strMeasure5) {
+        this.strMeasure5 = strMeasure5;
     }
 }
