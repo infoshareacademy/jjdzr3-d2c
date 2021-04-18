098051cf1cccce2658ba987cdba9fd6d30e8800a
Sandra Kowalska <sandrakowalskadev@gmail.com>
2021-04-18 10:37:12 +0200

    JJDZR3D2C-12: refactored code for searching items by drink name

--- src/main/java/Search.java
+++ src/main/java/Search.java
@@ -2,7 +2,6 @@ import domain.Drink;
 import domain.DrinkRepository;
 
 import java.util.List;
-import java.util.Locale;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
@@ -10,34 +9,39 @@ public class Search {
 
     public static void search(DrinkRepository drinkRepository) {
         Scanner scan = new Scanner(System.in);
-
-
         String input = "";
+
         while (input.length() < 3) {
             System.out.println("Specify search string");
             input = scan.nextLine();
 
-            if (input.length() >= 3) {
-                String finalInput = input;
-                List<Drink> filteredDrinks = drinkRepository.getDrinks()
-                        .stream()
-                        .filter(a -> a.getDrinkName().toLowerCase().contains(finalInput.toLowerCase()))
-                        .collect(Collectors.toList());
-
-                System.out.println();
-                if (filteredDrinks.size() == 0) {
-                    System.out.println("No items found");
-                } else {
-                    System.out.println("Found times are");
-                    for (Drink drink : filteredDrinks) {
-                        System.out.println(drink);
-                    }
-                }
+            if (input.trim().length() >= 3) {
+                searchItemsForQuery(drinkRepository, input);
             } else {
-                System.out.println("Too short search input");
+                System.out.println("Too short search criteria input");
             }
-        };
+        }
 
+    }
+
+    private static void searchItemsForQuery(DrinkRepository drinkRepository, String input) {
+        List<Drink> filteredDrinks = drinkRepository.getDrinks()
+                .stream()
+                .filter(a -> a.getDrinkName().toLowerCase().contains(input.toLowerCase()))
+                .collect(Collectors.toList());
+
+        if (filteredDrinks.size() == 0) {
+            System.out.println("No items found for given search criteria");
+        } else {
+            printOutFoundItems(filteredDrinks);
+        }
+    }
+
+    private static void printOutFoundItems(List<Drink> filteredDrinks) {
+        System.out.println("Items matching search criteria are: ");
+        for (Drink drink : filteredDrinks) {
+            System.out.println(drink);
+        }
     }
 
 }
